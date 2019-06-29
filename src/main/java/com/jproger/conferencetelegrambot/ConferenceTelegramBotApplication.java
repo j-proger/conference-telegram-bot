eb6a303061eb71cc26fac716b501cf43619c9c92
Skachkov Matvey <skachkov_matvey@mail.ru>
2019-06-29 11:32:46 +0300

    Init

---
+++ src/main/java/com/jproger/conferencetelegrambot/ConferenceTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.jproger.conferencetelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceTelegramBotApplication.class, args);
+	}
+
+}
