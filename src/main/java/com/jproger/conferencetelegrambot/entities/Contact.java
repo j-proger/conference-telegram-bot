16e905a16cc3282012f80d34e1a5c890c1e5b839
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2019-06-29 13:23:38 +0300

    Implement ConferenceController and ConferenceService

--- src/main/java/com/jproger/conferencetelegrambot/entities/Contact.java
+++ src/main/java/com/jproger/conferencetelegrambot/entities/Contact.java
@@ -2,9 +2,11 @@ package com.jproger.conferencetelegrambot.entities;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
 @Builder
+@Accessors(chain = true)
 public class Contact {
     private String name;
     private String phone;
