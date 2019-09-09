c81e191298bd239ebfc85ae6f0abf75bd1e04563
Skachkov Matvey <skachkov_matvey@mail.ru>
2019-09-09 21:29:53 +0300

    Add endpoint for getting feedback list

--- src/main/java/com/jproger/conferencetelegrambot/feedback/FeedbackService.java
+++ src/main/java/com/jproger/conferencetelegrambot/feedback/FeedbackService.java
@@ -1,11 +1,15 @@
 package com.jproger.conferencetelegrambot.feedback;
 
+import com.jproger.conferencetelegrambot.common.dto.PageRequestDto;
+import com.jproger.conferencetelegrambot.common.dto.PageResponseDto;
 import com.jproger.conferencetelegrambot.feedback.dto.FeedbackDto;
 import com.jproger.conferencetelegrambot.feedback.entities.Feedback;
 import com.jproger.conferencetelegrambot.feedback.mappers.FeedbackMapper;
 import com.jproger.conferencetelegrambot.feedback.repositories.FeedbackRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Nonnull;
@@ -27,5 +31,13 @@ public class FeedbackService {
         );
 
         return feedbackMapper.toFeedbackDto(feedback);
+    }
+
+    @Nonnull
+    public PageResponseDto<FeedbackDto> getFeedback(PageRequestDto pageRequest) {
+        Page<FeedbackDto> feedback = feedbackRepository.findAll(PageRequest.of(pageRequest.getPage(), pageRequest.getSize()))
+                .map(feedbackMapper::toFeedbackDto);
+
+        return PageResponseDto.of(feedback);
     }
 }
